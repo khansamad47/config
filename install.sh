c7259c3c55d97bfdf1990a99484c99b0fb9f7e5a
Samad Khan <khan.samad47@gmail.com>
2017-04-05 21:48:06 -0400

    Adding bashrc file

--- install.sh
+++ install.sh
@@ -2,3 +2,10 @@
 SCRIPT=$(dirname $(realpath $0))
 ln -s $SCRIPT/tmux.conf $HOME/.tmux.conf
 ln -s $SCRIPT/vimrc     $HOME/.vimrc
+
+if [ ! -f $HOME/.bashrc ]; then
+    echo "No bashrc file found!";
+    exit 1;
+fi
+
+echo "source $SCRIPT/bashrc" >> $HOME/.bashrc
--- setup_unix.sh
+++ setup_unix.sh
@@ -1,17 +1,15 @@
 sudo apt install git
-
 sudo apt install vim
 
-# Python Environment Manager
+# Python Stuff
 sudo apt install virtualenv
-
 sudo apt install spyder
 
 # LaTeX editor
 sudo apt install gummi
 
-mkdir ~/bin
-mkdir ~/tmp
+mkdir -vp $HOME/bin
+mkdir -vp $HOME/tmp
 
 # Directory for python environments
-mkdir ~/.virtualenv
+mkdir -vp $HOME/.virtualenv
