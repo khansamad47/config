f773b776cb2fef50e78ba5100a9b154841f8a319
Samad Khan <khan.samad47@gmail.com>
2017-04-29 21:11:09 -0400

    Adding conky and git config

--- setup_unix.sh
+++ setup_unix.sh
@@ -2,6 +2,9 @@ sudo apt install git
 sudo apt install vim
 sudo apt install tmux
 
+# Git setup
+git config --global core.editor "vim"
+
 # Python Stuff
 sudo apt install virtualenv
 sudo apt install spyder
@@ -17,3 +20,9 @@ mkdir -vp $HOME/.virtualenv
 
 # This is required so that tmux can use .bashrc
 echo 'source $HOME/.bashrc' >> $HOME/.bash_profile
+
+# Memory monitoring wiget
+sudo apt install conky-all
+sudo apt-add-repository -y ppa:teejee2008/ppa
+sudo apt-get update
+sudo apt-get install conky-manager # This is a ui to configure conky
