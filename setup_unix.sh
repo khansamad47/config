3a7b2510a6fd26987897d9d4e4dced0530992c91
Samad Khan <khan.samad47@gmail.com>
2025-12-06 12:12:44 -0500

    Use sudo for creating and moving nvim binary

--- setup_unix.sh
+++ setup_unix.sh
@@ -7,8 +7,8 @@ sudo apt install ripgrep
 
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
 chmod u+x nvim-linux-x86_64.appimage
-mkdir -p /opt/bin
-mv nvim-linux-x86_64.appimage /opt/bin/nvim
+sudo mkdir -p /opt/bin
+sudo mv nvim-linux-x86_64.appimage /opt/bin/nvim
 
 # fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
