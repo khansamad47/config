6e02743e34b430931bc15918d3748d89203d4f11
Samad Khan <khan.samad47@gmail.com>
2025-12-06 11:57:44 -0500

    Update setup_unix.sh

--- setup_unix.sh
+++ setup_unix.sh
@@ -3,36 +3,36 @@ RESTART_REQUIRED=0
 sudo apt install git
 sudo apt install vim
 sudo apt install tmux
+sudo apt install ripgrep
+
+# fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+yes | ~/.fzf/install 
+
+export FZF_BASE=~/.fzf
+
+# cppcodegen
+git clone git@bbgithub.dev.bloomberg.com:akhan323/cppcodegen.git /opt/cppcodegen
+ln -s /opt/cppcodegen/driver.py /bin/cppcodegen
+
+# when-changed
+wget https://github.com/joh/when-changed/archive/master.zip -P /tmp
+pip3.11 install /tmp/master.zip
+
+# jumpdir
+git clone https://github.com/khansamad47/jumpdir.git /opt/jumpdir
+
+# Setup neo-vim configuration
+git clone https://github.com/khansamad47/nvim.git ~/nvim
+mv ~/nvim  ~/.config/nvim
 
 # Git setup
 git config --global core.editor "vim"
 git config --global user.email "khan.samad47@gmail.com"
 git config --global user.name "Samad Khan"
 
-# Python Stuff
-sudo apt install virtualenv
-# Directory for python environments
-mkdir -vp $HOME/.virtualenv
-read -p "Do you want to install Sypder(Python IDE)? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    sudo apt install spyder
-fi
-
-# LaTeX editor
-read -p "Do you want to install Gummi(LaTeX IDE)? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    sudo apt install gummi
-fi
-
 mkdir -vp $HOME/bin
 mkdir -vp $HOME/tmp
-
-# This is required so that tmux can use .bashrc
-echo 'source $HOME/.bashrc' >> $HOME/.bash_profile
 
 # Memory monitoring wiget
 read -p "Do you want to install conky(memory monitoring widget)? " -n 1 -r
@@ -43,18 +43,6 @@ then
     sudo apt-add-repository -y ppa:teejee2008/ppa
     sudo apt-get update
     sudo apt-get install conky-manager # This is a ui to configure conky
-fi
-
-
-# Install jumpdir
-read -p "Do you want to install jumpdir? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    git clone https://github.com/khansamad47/jumpdir.git
-    ./jumpdir/install.sh
-    rm -rf jumpdir
-    $RESTART_REQUIRE=1
 fi
 
 echo "You may have to run source ~/.bashrc"
